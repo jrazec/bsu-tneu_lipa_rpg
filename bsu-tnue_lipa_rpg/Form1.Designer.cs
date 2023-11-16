e94d8c32cdd34b0b668aecad360ac6e7f2de299f
jrazec <johnrazecschool@gmail.com>
2023-11-16 12:08:01 +0800

    Create draft design for the login and main windows

--- bsu-tnue_lipa_rpg/Form1.Designer.cs
+++ bsu-tnue_lipa_rpg/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.login_btn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(255, 133);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(347, 78);
+            this.login_btn.TabIndex = 1;
+            this.login_btn.Text = "Log-in";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(255, 230);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(347, 77);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sign-up";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(835, 484);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.login_btn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +66,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ bsu-tnue_lipa_rpg/Log_in.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace bsu_tnue_lipa_rpg
+{
+    partial class Log_in
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sr_code_tbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password_tbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.back_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sr_code_tbox
+            // 
+            this.sr_code_tbox.Location = new System.Drawing.Point(245, 104);
+            this.sr_code_tbox.Name = "sr_code_tbox";
+            this.sr_code_tbox.Size = new System.Drawing.Size(100, 22);
+            this.sr_code_tbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "sr code:";
+            // 
+            // password_tbox
+            // 
+            this.password_tbox.Location = new System.Drawing.Point(245, 141);
+            this.password_tbox.Name = "password_tbox";
+            this.password_tbox.Size = new System.Drawing.Size(100, 22);
+            this.password_tbox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "password:";
+            // 
+            // back_btn
+            // 
+            this.back_btn.Location = new System.Drawing.Point(245, 198);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(75, 23);
+            this.back_btn.TabIndex = 4;
+            this.back_btn.Text = "back";
+            this.back_btn.UseVisualStyleBackColor = true;
+            this.back_btn.Click += new System.EventHandler(this.back_btn_Click);
+            // 
+            // Log_in
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.password_tbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sr_code_tbox);
+            this.Name = "Log_in";
+            this.Text = "Log_in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sr_code_tbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password_tbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button back_btn;
+    }
+}
