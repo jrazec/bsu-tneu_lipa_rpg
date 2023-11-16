376a3da65df7f5d0ea1a9ffc575e52047da6bab9
jrazec <johnrazecschool@gmail.com>
2023-11-16 12:05:56 +0800

    Create file and code for showing main form

---
+++ bsu-tnue_lipa_rpg/Log_in.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bsu_tnue_lipa_rpg
+{
+    public partial class Log_in : Form
+    {
+        public Log_in()
+        {
+            InitializeComponent();
+        }
+
+        private void back_btn_Click(object sender, EventArgs e)
+        {
+            Form1 mainWindow = new Form1();//not sure here, seems leik wrong
+            mainWindow.Show();
+        }
+    }
+}
