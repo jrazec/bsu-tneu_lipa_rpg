2e726acf624d61f37a7a514430752b513aa65280
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-19 06:52:00 +0800

    Add design loginAcc_btn in form1 designer

--- bsu-tnue_lipa_rpg/Form1.Designer.cs
+++ bsu-tnue_lipa_rpg/Form1.Designer.cs
@@ -37,19 +37,25 @@
             // loginAcc_btn
             // 
             this.loginAcc_btn.BackColor = System.Drawing.Color.Maroon;
-            this.loginAcc_btn.Location = new System.Drawing.Point(255, 133);
+            this.loginAcc_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginAcc_btn.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginAcc_btn.ForeColor = System.Drawing.Color.White;
+            this.loginAcc_btn.Location = new System.Drawing.Point(351, 200);
+            this.loginAcc_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginAcc_btn.Name = "loginAcc_btn";
-            this.loginAcc_btn.Size = new System.Drawing.Size(347, 78);
+            this.loginAcc_btn.Size = new System.Drawing.Size(477, 117);
             this.loginAcc_btn.TabIndex = 1;
-            this.loginAcc_btn.Text = "Log-in Account";
+            this.loginAcc_btn.Text = "LOGIN ACCOUNT";
             this.loginAcc_btn.UseVisualStyleBackColor = false;
             this.loginAcc_btn.Click += new System.EventHandler(this.loginAcc_btn_Click);
             // 
             // signupAcc_btn
             // 
-            this.signupAcc_btn.Location = new System.Drawing.Point(255, 230);
+            this.signupAcc_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signupAcc_btn.Location = new System.Drawing.Point(351, 345);
+            this.signupAcc_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.signupAcc_btn.Name = "signupAcc_btn";
-            this.signupAcc_btn.Size = new System.Drawing.Size(347, 77);
+            this.signupAcc_btn.Size = new System.Drawing.Size(477, 116);
             this.signupAcc_btn.TabIndex = 2;
             this.signupAcc_btn.Text = "Create Account";
             this.signupAcc_btn.UseVisualStyleBackColor = true;
@@ -57,9 +63,10 @@
             // 
             // set_up_btn
             // 
-            this.set_up_btn.Location = new System.Drawing.Point(740, 437);
+            this.set_up_btn.Location = new System.Drawing.Point(1018, 582);
+            this.set_up_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.set_up_btn.Name = "set_up_btn";
-            this.set_up_btn.Size = new System.Drawing.Size(75, 23);
+            this.set_up_btn.Size = new System.Drawing.Size(103, 35);
             this.set_up_btn.TabIndex = 3;
             this.set_up_btn.Text = "Set-Up";
             this.set_up_btn.UseVisualStyleBackColor = true;
@@ -68,22 +75,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(370, 442);
+            this.label1.Location = new System.Drawing.Point(501, 589);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(342, 16);
+            this.label1.Size = new System.Drawing.Size(520, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "Click Here first if it is your first time downloading the game";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 484);
+            this.ClientSize = new System.Drawing.Size(1151, 639);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.set_up_btn);
             this.Controls.Add(this.signupAcc_btn);
             this.Controls.Add(this.loginAcc_btn);
+            this.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
