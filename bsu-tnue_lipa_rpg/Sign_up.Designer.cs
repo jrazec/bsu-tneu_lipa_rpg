bc441748a660aff62427d04f43dc7715f2800546
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-19 10:55:56 +0800

    Add Picbox signuppng

--- bsu-tnue_lipa_rpg/Sign_up.Designer.cs
+++ bsu-tnue_lipa_rpg/Sign_up.Designer.cs
@@ -42,25 +42,32 @@
             this.label5 = new System.Windows.Forms.Label();
             this.password_validator_tbox = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(439, 488);
+            this.button1.Location = new System.Drawing.Point(448, 528);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 39);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 12;
             this.button1.Text = "back";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(577, 488);
+            this.button2.Location = new System.Drawing.Point(586, 528);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(117, 39);
-            this.button2.TabIndex = 1;
+            this.button2.TabIndex = 13;
             this.button2.Text = "continue";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -68,32 +75,32 @@
             // Crt_sr_code_tbox
             // 
             this.Crt_sr_code_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Crt_sr_code_tbox.Location = new System.Drawing.Point(332, 181);
+            this.Crt_sr_code_tbox.Location = new System.Drawing.Point(341, 231);
             this.Crt_sr_code_tbox.Name = "Crt_sr_code_tbox";
             this.Crt_sr_code_tbox.Size = new System.Drawing.Size(196, 41);
-            this.Crt_sr_code_tbox.TabIndex = 2;
+            this.Crt_sr_code_tbox.TabIndex = 0;
             // 
             // Crt_password_tbox
             // 
             this.Crt_password_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Crt_password_tbox.Location = new System.Drawing.Point(332, 359);
+            this.Crt_password_tbox.Location = new System.Drawing.Point(341, 409);
             this.Crt_password_tbox.Name = "Crt_password_tbox";
             this.Crt_password_tbox.Size = new System.Drawing.Size(196, 41);
-            this.Crt_password_tbox.TabIndex = 3;
+            this.Crt_password_tbox.TabIndex = 8;
             // 
             // Crt_first_name_tbox
             // 
             this.Crt_first_name_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Crt_first_name_tbox.Location = new System.Drawing.Point(605, 181);
+            this.Crt_first_name_tbox.Location = new System.Drawing.Point(614, 231);
             this.Crt_first_name_tbox.Name = "Crt_first_name_tbox";
             this.Crt_first_name_tbox.Size = new System.Drawing.Size(196, 41);
-            this.Crt_first_name_tbox.TabIndex = 3;
+            this.Crt_first_name_tbox.TabIndex = 1;
             this.Crt_first_name_tbox.TextChanged += new System.EventHandler(this.Crt_first_name_tbox_TextChanged);
             // 
             // Crt_last_name_tbox
             // 
             this.Crt_last_name_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Crt_last_name_tbox.Location = new System.Drawing.Point(332, 270);
+            this.Crt_last_name_tbox.Location = new System.Drawing.Point(341, 320);
             this.Crt_last_name_tbox.Name = "Crt_last_name_tbox";
             this.Crt_last_name_tbox.Size = new System.Drawing.Size(196, 41);
             this.Crt_last_name_tbox.TabIndex = 4;
@@ -101,7 +108,7 @@
             // Crt_gender_tbox
             // 
             this.Crt_gender_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Crt_gender_tbox.Location = new System.Drawing.Point(605, 270);
+            this.Crt_gender_tbox.Location = new System.Drawing.Point(614, 320);
             this.Crt_gender_tbox.Name = "Crt_gender_tbox";
             this.Crt_gender_tbox.Size = new System.Drawing.Size(196, 41);
             this.Crt_gender_tbox.TabIndex = 5;
@@ -112,22 +119,22 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(357, 227);
+            this.label1.Location = new System.Drawing.Point(366, 277);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 22);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Enter your sr_code:";
+            this.label1.Size = new System.Drawing.Size(139, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter your sr code";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(659, 227);
+            this.label2.Location = new System.Drawing.Point(668, 277);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 22);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "First name:";
+            this.label2.Size = new System.Drawing.Size(84, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First name";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
@@ -135,11 +142,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(388, 316);
+            this.label3.Location = new System.Drawing.Point(397, 366);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 22);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Last name:";
+            this.label3.Size = new System.Drawing.Size(81, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last name";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
@@ -147,11 +154,11 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(672, 316);
+            this.label4.Location = new System.Drawing.Point(681, 366);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 22);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Gender:";
+            this.label4.Size = new System.Drawing.Size(60, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Gender";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
@@ -159,19 +166,19 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(389, 405);
+            this.label5.Location = new System.Drawing.Point(398, 455);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 22);
+            this.label5.Size = new System.Drawing.Size(79, 22);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Password:";
+            this.label5.Text = "Password";
             // 
             // password_validator_tbox
             // 
             this.password_validator_tbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.password_validator_tbox.Location = new System.Drawing.Point(605, 359);
+            this.password_validator_tbox.Location = new System.Drawing.Point(614, 409);
             this.password_validator_tbox.Name = "password_validator_tbox";
             this.password_validator_tbox.Size = new System.Drawing.Size(196, 41);
-            this.password_validator_tbox.TabIndex = 11;
+            this.password_validator_tbox.TabIndex = 9;
             this.password_validator_tbox.TextChanged += new System.EventHandler(this.password_validator_tbox_TextChanged);
             // 
             // label6
@@ -179,19 +186,64 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(630, 405);
+            this.label6.Location = new System.Drawing.Point(639, 455);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(144, 22);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Re-Type Password:";
+            this.label6.Size = new System.Drawing.Size(140, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Re-Type Password";
             this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(43, 604);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 26);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "RazBerYie";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.SIGNUPPNG;
+            this.pictureBox3.Location = new System.Drawing.Point(373, 65);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(423, 125);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::bsu_tnue_lipa_rpg.Properties.Resources._111_01;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 604);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.Exit_Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(1103, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // Sign_up
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(1133, 592);
+            this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.password_validator_tbox);
             this.Controls.Add(this.label5);
@@ -206,10 +258,14 @@
             this.Controls.Add(this.Crt_sr_code_tbox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Sign_up";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sign_up";
             this.Load += new System.EventHandler(this.Sign_up_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -231,5 +287,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox password_validator_tbox;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
