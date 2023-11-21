a38b4963184c843142aef1b38150c2804e886174
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-21 09:16:16 +0800

    Add t3_pbox

--- bsu-tnue_lipa_rpg/Game_mechanics.Designer.cs
+++ bsu-tnue_lipa_rpg/Game_mechanics.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.t4_pbox = new System.Windows.Forms.PictureBox();
+            this.t3_pbox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.t4_pbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3_pbox)).BeginInit();
             this.SuspendLayout();
             // 
             // t4_pbox
@@ -42,6 +44,19 @@
             this.t4_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.t4_pbox.TabIndex = 0;
             this.t4_pbox.TabStop = false;
+            this.t4_pbox.Visible = false;
+            // 
+            // t3_pbox
+            // 
+            this.t3_pbox.BackColor = System.Drawing.Color.Transparent;
+            this.t3_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.tutorial_3;
+            this.t3_pbox.Location = new System.Drawing.Point(39, 12);
+            this.t3_pbox.Name = "t3_pbox";
+            this.t3_pbox.Size = new System.Drawing.Size(1073, 601);
+            this.t3_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.t3_pbox.TabIndex = 1;
+            this.t3_pbox.TabStop = false;
+            this.t3_pbox.Visible = false;
             // 
             // Game_mechanics
             // 
@@ -49,6 +64,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::bsu_tnue_lipa_rpg.Properties.Resources.map_blur;
             this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.t3_pbox);
             this.Controls.Add(this.t4_pbox);
             this.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -56,6 +72,7 @@
             this.Name = "Game_mechanics";
             this.Text = "Game_mechanics";
             ((System.ComponentModel.ISupportInitialize)(this.t4_pbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t3_pbox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -63,5 +80,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox t4_pbox;
+        private System.Windows.Forms.PictureBox t3_pbox;
     }
 }
