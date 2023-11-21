09b9ef4227909bf8e5a89b3b4aeb650c6c3e79b5
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-21 13:07:29 +0800

    Add bg image bedroom

---
+++ bsu-tnue_lipa_rpg/Character_sel.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace bsu_tnue_lipa_rpg
+{
+    partial class Character_sel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Character_sel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::bsu_tnue_lipa_rpg.Properties.Resources.bedroom;
+            this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Character_sel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Character_sel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
