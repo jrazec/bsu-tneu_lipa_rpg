163e721ae0a412c4190da3b8ba2e629fb7a86f4f
Bernadette Awatin <badet.awatin@gmail.com>
2023-11-25 18:08:27 +0800

    Add map scene images

---
+++ bsu-tnue_lipa_rpg/Map.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace bsu_tnue_lipa_rpg
+{
+    partial class Map
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.map_charac = new System.Windows.Forms.PictureBox();
+            this.mapWalkTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.map_charac)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1151, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // map_charac
+            // 
+            this.map_charac.BackColor = System.Drawing.Color.Transparent;
+            this.map_charac.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.male_uni_front;
+            this.map_charac.Location = new System.Drawing.Point(509, 350);
+            this.map_charac.Name = "map_charac";
+            this.map_charac.Size = new System.Drawing.Size(53, 86);
+            this.map_charac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.map_charac.TabIndex = 1;
+            this.map_charac.TabStop = false;
+            // 
+            // mapWalkTimer
+            // 
+            this.mapWalkTimer.Interval = 50;
+            this.mapWalkTimer.Tick += new System.EventHandler(this.mapWalkTimer_Tick);
+            // 
+            // Map
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::bsu_tnue_lipa_rpg.Properties.Resources.map_art;
+            this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.map_charac);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Map";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Map";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.key_is_down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.key_is_up);
+            ((System.ComponentModel.ISupportInitialize)(this.map_charac)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox map_charac;
+        private System.Windows.Forms.Timer mapWalkTimer;
+    }
+}
---
+++ bsu-tnue_lipa_rpg/Map.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bsu_tnue_lipa_rpg
+{
+    public partial class Map : Form
+    {
+        bool go_up, go_down, go_left, go_right;
+        int walk = 20;
+        public Map()
+        {
+            InitializeComponent();
+        }
+
+        private void mapWalkTimer_Tick(object sender, EventArgs e)
+        {
+            if (go_left == true && map_charac.Left > 149)
+            {
+                map_charac.Left -= walk;
+            }
+            if (go_right == true && map_charac.Left + map_charac.Width < this.ClientSize.Width)
+            {
+                map_charac.Left += walk;
+            }
+            if (go_up == true && map_charac.Top > 46)
+            {
+                map_charac.Top -= walk;
+            }
+            if (go_down == true && map_charac.Top + map_charac.Height < this.ClientSize.Height)
+            {
+                map_charac.Top += walk;
+            }
+        }
+
+        private void key_is_down(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                e.Handled = true;
+                go_left = true;
+                map_charac.Image = Properties.Resources.male_uni_left;
+            }
+
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                e.Handled = true;
+                go_right = true;
+                map_charac.Image = Properties.Resources.male_uni_right;
+            }
+
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                e.Handled = true;
+                go_up = true;
+                map_charac.Image = Properties.Resources.male_uni_back;
+            }
+
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                e.Handled = true;
+                go_down = true;
+                map_charac.Image = Properties.Resources.male_uni_front;
+            }
+        }
+
+        private void key_is_up(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                go_left = false;
+            }
+
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                go_right = false;
+            }
+
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                go_up = false;
+            }
+
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                go_down = false;
+            }
+        }
+    }
+}
