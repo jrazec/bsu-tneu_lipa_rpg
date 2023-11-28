29bb396a447d05ddf5f52359023aee3f657b8ab7
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-28 11:36:47 +0800

    Add create_pbox

--- bsu-tnue_lipa_rpg/Admin_Create.Designer.cs
+++ bsu-tnue_lipa_rpg/Admin_Create.Designer.cs
@@ -28,13 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin_Create));
-            this.label9 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
             this.search_txt = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.name_ign_lbl = new System.Windows.Forms.Label();
@@ -45,14 +38,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.home_lbl = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.exit_btn = new System.Windows.Forms.Button();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
             this.search_btn = new System.Windows.Forms.Button();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.id_pbox = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
@@ -60,15 +49,11 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel5.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.create_pbox = new System.Windows.Forms.PictureBox();
+            this.create_btn = new System.Windows.Forms.Button();
             this.panel3.SuspendLayout();
-            this.panel6.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.id_pbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -76,67 +61,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.create_pbox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label9
-            // 
-            this.label9.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(19, 75);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(165, 161);
-            this.label9.TabIndex = 17;
-            this.label9.Text = resources.GetString("label9.Text");
-            // 
-            // label8
-            // 
-            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(18, 75);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(165, 161);
-            this.label8.TabIndex = 16;
-            this.label8.Text = resources.GetString("label8.Text");
-            // 
-            // label7
-            // 
-            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(15, 75);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(165, 161);
-            this.label7.TabIndex = 15;
-            this.label7.Text = resources.GetString("label7.Text");
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.LightCoral;
-            this.panel5.Controls.Add(this.label8);
-            this.panel5.Controls.Add(this.pictureBox9);
-            this.panel5.Location = new System.Drawing.Point(592, 299);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(197, 258);
-            this.panel5.TabIndex = 18;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.Controls.Add(this.label7);
-            this.panel4.Controls.Add(this.pictureBox8);
-            this.panel4.Location = new System.Drawing.Point(346, 299);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(197, 258);
-            this.panel4.TabIndex = 17;
-            // 
-            // label6
-            // 
-            this.label6.BackColor = System.Drawing.Color.LightCoral;
-            this.label6.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(344, 95);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(692, 173);
-            this.label6.TabIndex = 16;
-            this.label6.Text = resources.GetString("label6.Text");
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // search_txt
             // 
@@ -212,7 +138,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Maroon;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(99, 287);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(84, 34);
@@ -222,7 +150,7 @@
             // home_lbl
             // 
             this.home_lbl.AutoSize = true;
-            this.home_lbl.BackColor = System.Drawing.Color.Maroon;
+            this.home_lbl.BackColor = System.Drawing.Color.White;
             this.home_lbl.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.home_lbl.ForeColor = System.Drawing.Color.Black;
             this.home_lbl.Location = new System.Drawing.Point(99, 224);
@@ -234,20 +162,10 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Maroon;
-            this.panel2.Location = new System.Drawing.Point(3, 215);
+            this.panel2.Location = new System.Drawing.Point(3, 278);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(243, 55);
             this.panel2.TabIndex = 12;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.Color.White;
-            this.panel6.Controls.Add(this.label9);
-            this.panel6.Controls.Add(this.pictureBox10);
-            this.panel6.Location = new System.Drawing.Point(838, 299);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(197, 258);
-            this.panel6.TabIndex = 19;
             // 
             // panel1
             // 
@@ -286,26 +204,6 @@
             this.exit_btn.TabIndex = 20;
             this.exit_btn.UseVisualStyleBackColor = false;
             // 
-            // pictureBox9
-            // 
-            this.pictureBox9.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.female_org_front;
-            this.pictureBox9.Location = new System.Drawing.Point(67, 12);
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(64, 52);
-            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox9.TabIndex = 15;
-            this.pictureBox9.TabStop = false;
-            // 
-            // pictureBox8
-            // 
-            this.pictureBox8.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.male_org_front;
-            this.pictureBox8.Location = new System.Drawing.Point(63, 12);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(64, 52);
-            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox8.TabIndex = 14;
-            this.pictureBox8.TabStop = false;
-            // 
             // search_btn
             // 
             this.search_btn.BackgroundImage = global::bsu_tnue_lipa_rpg.Properties.Resources.search;
@@ -326,16 +224,6 @@
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 10;
             this.pictureBox7.TabStop = false;
-            // 
-            // pictureBox10
-            // 
-            this.pictureBox10.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.male_org_front;
-            this.pictureBox10.Location = new System.Drawing.Point(63, 12);
-            this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(64, 52);
-            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox10.TabIndex = 15;
-            this.pictureBox10.TabStop = false;
             // 
             // id_pbox
             // 
@@ -390,7 +278,8 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.select;
+            this.pictureBox2.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox2.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.select1;
             this.pictureBox2.Location = new System.Drawing.Point(64, 291);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(25, 25);
@@ -400,7 +289,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.home;
             this.pictureBox1.Location = new System.Drawing.Point(64, 227);
             this.pictureBox1.Name = "pictureBox1";
@@ -409,33 +298,43 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // create_pbox
+            // 
+            this.create_pbox.Location = new System.Drawing.Point(338, 105);
+            this.create_pbox.Name = "create_pbox";
+            this.create_pbox.Size = new System.Drawing.Size(705, 436);
+            this.create_pbox.TabIndex = 21;
+            this.create_pbox.TabStop = false;
+            // 
+            // create_btn
+            // 
+            this.create_btn.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.create_btn.Location = new System.Drawing.Point(338, 560);
+            this.create_btn.Name = "create_btn";
+            this.create_btn.Size = new System.Drawing.Size(88, 29);
+            this.create_btn.TabIndex = 22;
+            this.create_btn.Text = "CREATE";
+            this.create_btn.UseVisualStyleBackColor = true;
+            // 
             // Admin_Create
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
+            this.Controls.Add(this.create_btn);
+            this.Controls.Add(this.create_pbox);
             this.Controls.Add(this.exit_btn);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel6);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Admin_Create";
             this.Size = new System.Drawing.Size(1151, 639);
-            this.panel5.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            this.panel6.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.id_pbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
@@ -443,6 +342,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.create_pbox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -457,17 +357,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button search_btn;
         private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.PictureBox pictureBox9;
-        private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.Button exit_btn;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.PictureBox pictureBox8;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox search_txt;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label name_ign_lbl;
@@ -478,7 +369,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label home_lbl;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox create_pbox;
+        private System.Windows.Forms.Button create_btn;
     }
 }
