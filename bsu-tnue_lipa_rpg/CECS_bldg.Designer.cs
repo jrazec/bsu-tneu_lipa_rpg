4d1438076d99c3eba09628b15f067ea35ca9817a
Bernadette Awatin <badet.awatin@gmail.com>
2023-11-28 09:08:34 +0800

    Add form to contain CECS bldg related user controls

---
+++ bsu-tnue_lipa_rpg/CECS_bldg.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace bsu_tnue_lipa_rpg
+{
+    partial class CECS_bldg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cecscontainer_panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1151, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // cecscontainer_panel
+            // 
+            this.cecscontainer_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cecscontainer_panel.Location = new System.Drawing.Point(0, 50);
+            this.cecscontainer_panel.Name = "cecscontainer_panel";
+            this.cecscontainer_panel.Size = new System.Drawing.Size(1151, 589);
+            this.cecscontainer_panel.TabIndex = 1;
+            // 
+            // CECS_bldg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.cecscontainer_panel);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CECS_bldg";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CECS_bldg";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel cecscontainer_panel;
+    }
+}
