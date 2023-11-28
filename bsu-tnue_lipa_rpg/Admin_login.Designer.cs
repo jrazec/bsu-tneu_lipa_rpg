7edd48e42c676023f408f2c939f09a61e7fe1a0f
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-28 10:00:12 +0800

    Add the admin_user_txt and admin_pass-txt

--- bsu-tnue_lipa_rpg/Admin_login.Designer.cs
+++ bsu-tnue_lipa_rpg/Admin_login.Designer.cs
@@ -28,7 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.admin_user_txt = new System.Windows.Forms.TextBox();
+            this.admin_pass_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // admin_user_txt
+            // 
+            this.admin_user_txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_user_txt.Location = new System.Drawing.Point(439, 262);
+            this.admin_user_txt.Name = "admin_user_txt";
+            this.admin_user_txt.Size = new System.Drawing.Size(272, 40);
+            this.admin_user_txt.TabIndex = 0;
+            // 
+            // admin_pass_txt
+            // 
+            this.admin_pass_txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_pass_txt.Location = new System.Drawing.Point(439, 345);
+            this.admin_pass_txt.Name = "admin_pass_txt";
+            this.admin_pass_txt.Size = new System.Drawing.Size(272, 40);
+            this.admin_pass_txt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(43, 604);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RazBerYie";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources._111_01;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 604);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Admin_login
             // 
@@ -36,16 +78,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.admin_pass_txt);
+            this.Controls.Add(this.admin_user_txt);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Admin_login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox admin_user_txt;
+        private System.Windows.Forms.TextBox admin_pass_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
