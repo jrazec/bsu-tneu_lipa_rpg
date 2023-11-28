ab9d92c22a98c3fe2d6efcdc861fb5ea7fa947ff
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-28 10:58:34 +0800

    Add id_pbox, name_ign_lbl

--- bsu-tnue_lipa_rpg/Admin_section.Designer.cs
+++ bsu-tnue_lipa_rpg/Admin_section.Designer.cs
@@ -39,17 +39,28 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.id_pbox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.name_ign_lbl = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.id_pbox)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.name_ign_lbl);
+            this.panel1.Controls.Add(this.id_pbox);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.pictureBox5);
             this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Controls.Add(this.pictureBox3);
@@ -60,6 +71,7 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.home_lbl);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(-4, -3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(246, 645);
@@ -68,57 +80,60 @@
             // home_lbl
             // 
             this.home_lbl.AutoSize = true;
-            this.home_lbl.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.home_lbl.Location = new System.Drawing.Point(101, 151);
+            this.home_lbl.BackColor = System.Drawing.Color.Maroon;
+            this.home_lbl.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.home_lbl.ForeColor = System.Drawing.Color.White;
+            this.home_lbl.Location = new System.Drawing.Point(99, 191);
             this.home_lbl.Name = "home_lbl";
-            this.home_lbl.Size = new System.Drawing.Size(78, 34);
+            this.home_lbl.Size = new System.Drawing.Size(67, 34);
             this.home_lbl.TabIndex = 0;
             this.home_lbl.Text = "HOME";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(101, 231);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 254);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 34);
+            this.label1.Size = new System.Drawing.Size(84, 34);
             this.label1.TabIndex = 1;
             this.label1.Text = "CREATE";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(103, 311);
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 317);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 34);
+            this.label2.Size = new System.Drawing.Size(63, 34);
             this.label2.TabIndex = 2;
             this.label2.Text = "READ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(101, 391);
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(99, 380);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 34);
+            this.label3.Size = new System.Drawing.Size(83, 34);
             this.label3.TabIndex = 3;
             this.label3.Text = "UPDATE";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(101, 471);
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(100, 443);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(99, 34);
+            this.label4.Size = new System.Drawing.Size(82, 34);
             this.label4.TabIndex = 4;
             this.label4.Text = "DELETE";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.home;
-            this.pictureBox1.Location = new System.Drawing.Point(66, 155);
+            this.pictureBox1.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.home1;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 194);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(25, 25);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -128,7 +143,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.select;
-            this.pictureBox2.Location = new System.Drawing.Point(66, 236);
+            this.pictureBox2.Location = new System.Drawing.Point(64, 258);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(25, 25);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -138,7 +153,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.search;
-            this.pictureBox3.Location = new System.Drawing.Point(66, 315);
+            this.pictureBox3.Location = new System.Drawing.Point(62, 321);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(25, 25);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -148,7 +163,7 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.cloud_upload_alt;
-            this.pictureBox4.Location = new System.Drawing.Point(66, 395);
+            this.pictureBox4.Location = new System.Drawing.Point(64, 384);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(25, 25);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -158,12 +173,62 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.trash;
-            this.pictureBox5.Location = new System.Drawing.Point(66, 476);
+            this.pictureBox5.Location = new System.Drawing.Point(65, 447);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(25, 25);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 8;
             this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox6.Image = global::bsu_tnue_lipa_rpg.Properties.Resources._111_01;
+            this.pictureBox6.Location = new System.Drawing.Point(16, 607);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(29, 25);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(47, 607);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "RazBerYie";
+            // 
+            // id_pbox
+            // 
+            this.id_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.male_org_front;
+            this.id_pbox.Location = new System.Drawing.Point(62, 28);
+            this.id_pbox.Name = "id_pbox";
+            this.id_pbox.Size = new System.Drawing.Size(116, 107);
+            this.id_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.id_pbox.TabIndex = 11;
+            this.id_pbox.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Location = new System.Drawing.Point(3, 182);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(243, 55);
+            this.panel2.TabIndex = 12;
+            // 
+            // name_ign_lbl
+            // 
+            this.name_ign_lbl.AutoSize = true;
+            this.name_ign_lbl.ForeColor = System.Drawing.Color.Black;
+            this.name_ign_lbl.Location = new System.Drawing.Point(72, 147);
+            this.name_ign_lbl.Name = "name_ign_lbl";
+            this.name_ign_lbl.Size = new System.Drawing.Size(90, 22);
+            this.name_ign_lbl.TabIndex = 13;
+            this.name_ign_lbl.Text = "name(ign)";
             // 
             // Admin_section
             // 
@@ -184,6 +249,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.id_pbox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -201,5 +268,10 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox id_pbox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label name_ign_lbl;
     }
 }
