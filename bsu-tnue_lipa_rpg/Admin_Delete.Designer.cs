49eebd2fbcff2403647da214647aae692c96e4d5
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-28 16:59:49 +0800

    Create Admin_Delete user control

---
+++ bsu-tnue_lipa_rpg/Admin_Delete.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace bsu_tnue_lipa_rpg
+{
+    partial class Admin_Delete
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Admin_Delete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "Admin_Delete";
+            this.Size = new System.Drawing.Size(1151, 639);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
