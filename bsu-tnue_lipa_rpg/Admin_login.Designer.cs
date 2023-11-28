d13fd3de0463bcf415afa2a5586405327d7d7745
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-28 10:11:27 +0800

    Add pbox exit

--- bsu-tnue_lipa_rpg/Admin_login.Designer.cs
+++ bsu-tnue_lipa_rpg/Admin_login.Designer.cs
@@ -35,12 +35,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // admin_user_txt
             // 
+            this.admin_user_txt.BackColor = System.Drawing.Color.Firebrick;
             this.admin_user_txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_user_txt.ForeColor = System.Drawing.Color.White;
             this.admin_user_txt.Location = new System.Drawing.Point(439, 262);
             this.admin_user_txt.Name = "admin_user_txt";
             this.admin_user_txt.Size = new System.Drawing.Size(272, 40);
@@ -48,7 +52,9 @@
             // 
             // admin_pass_txt
             // 
+            this.admin_pass_txt.BackColor = System.Drawing.Color.Firebrick;
             this.admin_pass_txt.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_pass_txt.ForeColor = System.Drawing.Color.White;
             this.admin_pass_txt.Location = new System.Drawing.Point(439, 345);
             this.admin_pass_txt.Name = "admin_pass_txt";
             this.admin_pass_txt.Size = new System.Drawing.Size(272, 40);
@@ -58,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
             this.label1.Location = new System.Drawing.Point(43, 604);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 26);
@@ -67,6 +73,7 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Maroon;
             this.pictureBox1.Image = global::bsu_tnue_lipa_rpg.Properties.Resources._111_01;
             this.pictureBox1.Location = new System.Drawing.Point(12, 604);
             this.pictureBox1.Name = "pictureBox1";
@@ -79,7 +86,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(540, 308);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 22);
@@ -90,7 +97,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(531, 402);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(89, 22);
@@ -100,19 +107,32 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(415, 92);
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(392, 92);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(308, 144);
+            this.label4.Size = new System.Drawing.Size(366, 144);
             this.label4.TabIndex = 7;
             this.label4.Text = "ADMIN";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox2.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.Exit_Icon;
+            this.pictureBox2.Location = new System.Drawing.Point(1103, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
             // 
             // Admin_login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1151, 639);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -127,6 +147,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,5 +162,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
