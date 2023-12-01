6bfe14d01bb5de984f18eb52db7a56ef2b9a5ef8
Bernadette Awatin <badet.awatin@gmail.com>
2023-12-01 18:14:23 +0800

    Add menu, hint, and hover events to Old bldg form

--- bsu-tnue_lipa_rpg/Old_Bldg.Designer.cs
+++ bsu-tnue_lipa_rpg/Old_Bldg.Designer.cs
@@ -30,11 +30,44 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.obcontainer_panel = new System.Windows.Forms.Panel();
+            this.viewmenu_panel = new System.Windows.Forms.Panel();
+            this.return_panel = new System.Windows.Forms.Panel();
+            this.return_label = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.achievs_panel = new System.Windows.Forms.Panel();
+            this.achievs_lbl = new System.Windows.Forms.Label();
+            this.tasks_panel = new System.Windows.Forms.Panel();
+            this.tasks_lbl = new System.Windows.Forms.Label();
+            this.profile_panel = new System.Windows.Forms.Panel();
+            this.ign_lbl = new System.Windows.Forms.Label();
+            this.hint_panel = new System.Windows.Forms.Panel();
+            this.hinttext_lbl = new System.Windows.Forms.Label();
+            this.menu_pbox = new System.Windows.Forms.PictureBox();
+            this.day_lbl = new System.Windows.Forms.Label();
+            this.currency_lbl = new System.Windows.Forms.Label();
+            this.hint_pbox = new System.Windows.Forms.PictureBox();
+            this.hint_lbl = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.viewmenu_panel.SuspendLayout();
+            this.return_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.achievs_panel.SuspendLayout();
+            this.tasks_panel.SuspendLayout();
+            this.profile_panel.SuspendLayout();
+            this.hint_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.menu_pbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hint_pbox)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.hint_lbl);
+            this.panel1.Controls.Add(this.hint_pbox);
+            this.panel1.Controls.Add(this.currency_lbl);
+            this.panel1.Controls.Add(this.day_lbl);
+            this.panel1.Controls.Add(this.menu_pbox);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -43,25 +76,240 @@
             // 
             // obcontainer_panel
             // 
+            this.obcontainer_panel.BackColor = System.Drawing.Color.White;
             this.obcontainer_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.obcontainer_panel.Location = new System.Drawing.Point(0, 50);
             this.obcontainer_panel.Name = "obcontainer_panel";
             this.obcontainer_panel.Size = new System.Drawing.Size(1436, 589);
             this.obcontainer_panel.TabIndex = 1;
             // 
+            // viewmenu_panel
+            // 
+            this.viewmenu_panel.BackColor = System.Drawing.Color.White;
+            this.viewmenu_panel.Controls.Add(this.return_panel);
+            this.viewmenu_panel.Controls.Add(this.pictureBox6);
+            this.viewmenu_panel.Controls.Add(this.label5);
+            this.viewmenu_panel.Controls.Add(this.achievs_panel);
+            this.viewmenu_panel.Controls.Add(this.tasks_panel);
+            this.viewmenu_panel.Controls.Add(this.profile_panel);
+            this.viewmenu_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.viewmenu_panel.Location = new System.Drawing.Point(0, 50);
+            this.viewmenu_panel.Name = "viewmenu_panel";
+            this.viewmenu_panel.Size = new System.Drawing.Size(208, 589);
+            this.viewmenu_panel.TabIndex = 19;
+            this.viewmenu_panel.Visible = false;
+            // 
+            // return_panel
+            // 
+            this.return_panel.Controls.Add(this.return_label);
+            this.return_panel.Location = new System.Drawing.Point(0, 333);
+            this.return_panel.Name = "return_panel";
+            this.return_panel.Size = new System.Drawing.Size(208, 59);
+            this.return_panel.TabIndex = 19;
+            this.return_panel.MouseEnter += new System.EventHandler(this.return_hoverin);
+            this.return_panel.MouseLeave += new System.EventHandler(this.return_hoverout);
+            // 
+            // return_label
+            // 
+            this.return_label.AutoSize = true;
+            this.return_label.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.return_label.Location = new System.Drawing.Point(59, 19);
+            this.return_label.Name = "return_label";
+            this.return_label.Size = new System.Drawing.Size(93, 24);
+            this.return_label.TabIndex = 23;
+            this.return_label.Text = "Main Menu";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox6.Image = global::bsu_tnue_lipa_rpg.Properties.Resources._111_01;
+            this.pictureBox6.Location = new System.Drawing.Point(18, 555);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(29, 25);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 21;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(55, 555);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 26);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "RazBerYie";
+            // 
+            // achievs_panel
+            // 
+            this.achievs_panel.Controls.Add(this.achievs_lbl);
+            this.achievs_panel.Location = new System.Drawing.Point(0, 255);
+            this.achievs_panel.Name = "achievs_panel";
+            this.achievs_panel.Size = new System.Drawing.Size(208, 59);
+            this.achievs_panel.TabIndex = 18;
+            this.achievs_panel.MouseEnter += new System.EventHandler(this.achievs_hoverin);
+            this.achievs_panel.MouseLeave += new System.EventHandler(this.achievs_hoverout);
+            // 
+            // achievs_lbl
+            // 
+            this.achievs_lbl.AutoSize = true;
+            this.achievs_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.achievs_lbl.Location = new System.Drawing.Point(47, 18);
+            this.achievs_lbl.Name = "achievs_lbl";
+            this.achievs_lbl.Size = new System.Drawing.Size(120, 24);
+            this.achievs_lbl.TabIndex = 22;
+            this.achievs_lbl.Text = "Achievements";
+            // 
+            // tasks_panel
+            // 
+            this.tasks_panel.Controls.Add(this.tasks_lbl);
+            this.tasks_panel.Location = new System.Drawing.Point(0, 178);
+            this.tasks_panel.Name = "tasks_panel";
+            this.tasks_panel.Size = new System.Drawing.Size(208, 59);
+            this.tasks_panel.TabIndex = 17;
+            this.tasks_panel.MouseEnter += new System.EventHandler(this.tasks_hoverin);
+            this.tasks_panel.MouseLeave += new System.EventHandler(this.tasks_hoverout);
+            // 
+            // tasks_lbl
+            // 
+            this.tasks_lbl.AutoSize = true;
+            this.tasks_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tasks_lbl.Location = new System.Drawing.Point(77, 15);
+            this.tasks_lbl.Name = "tasks_lbl";
+            this.tasks_lbl.Size = new System.Drawing.Size(53, 24);
+            this.tasks_lbl.TabIndex = 0;
+            this.tasks_lbl.Text = "Tasks";
+            // 
+            // profile_panel
+            // 
+            this.profile_panel.BackColor = System.Drawing.Color.Maroon;
+            this.profile_panel.Controls.Add(this.ign_lbl);
+            this.profile_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.profile_panel.Location = new System.Drawing.Point(0, 0);
+            this.profile_panel.Name = "profile_panel";
+            this.profile_panel.Size = new System.Drawing.Size(208, 127);
+            this.profile_panel.TabIndex = 16;
+            // 
+            // ign_lbl
+            // 
+            this.ign_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ign_lbl.ForeColor = System.Drawing.Color.White;
+            this.ign_lbl.Location = new System.Drawing.Point(12, 23);
+            this.ign_lbl.Name = "ign_lbl";
+            this.ign_lbl.Size = new System.Drawing.Size(182, 68);
+            this.ign_lbl.TabIndex = 0;
+            this.ign_lbl.Text = "Hello, [IGN]\r\n\r\n[fullname] | [sr-code]";
+            this.ign_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hint_panel
+            // 
+            this.hint_panel.BackColor = System.Drawing.Color.White;
+            this.hint_panel.Controls.Add(this.hinttext_lbl);
+            this.hint_panel.Location = new System.Drawing.Point(1070, 50);
+            this.hint_panel.Name = "hint_panel";
+            this.hint_panel.Size = new System.Drawing.Size(366, 104);
+            this.hint_panel.TabIndex = 20;
+            this.hint_panel.Visible = false;
+            // 
+            // hinttext_lbl
+            // 
+            this.hinttext_lbl.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hinttext_lbl.ForeColor = System.Drawing.Color.Maroon;
+            this.hinttext_lbl.Location = new System.Drawing.Point(14, 12);
+            this.hinttext_lbl.Name = "hinttext_lbl";
+            this.hinttext_lbl.Size = new System.Drawing.Size(341, 79);
+            this.hinttext_lbl.TabIndex = 0;
+            this.hinttext_lbl.Text = "Hint goes here...";
+            this.hinttext_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menu_pbox
+            // 
+            this.menu_pbox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menu_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.menu_icon;
+            this.menu_pbox.Location = new System.Drawing.Point(10, 8);
+            this.menu_pbox.Name = "menu_pbox";
+            this.menu_pbox.Size = new System.Drawing.Size(44, 34);
+            this.menu_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.menu_pbox.TabIndex = 4;
+            this.menu_pbox.TabStop = false;
+            this.menu_pbox.Click += new System.EventHandler(this.menu_pbox_Click);
+            // 
+            // day_lbl
+            // 
+            this.day_lbl.AutoSize = true;
+            this.day_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.day_lbl.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.day_lbl.Location = new System.Drawing.Point(147, 9);
+            this.day_lbl.Name = "day_lbl";
+            this.day_lbl.Size = new System.Drawing.Size(99, 34);
+            this.day_lbl.TabIndex = 17;
+            this.day_lbl.Text = "Monday";
+            // 
+            // currency_lbl
+            // 
+            this.currency_lbl.AutoSize = true;
+            this.currency_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currency_lbl.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.currency_lbl.Location = new System.Drawing.Point(1061, 8);
+            this.currency_lbl.Name = "currency_lbl";
+            this.currency_lbl.Size = new System.Drawing.Size(83, 34);
+            this.currency_lbl.TabIndex = 18;
+            this.currency_lbl.Text = "₱ 0.00";
+            // 
+            // hint_pbox
+            // 
+            this.hint_pbox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.hint_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.hint_icon;
+            this.hint_pbox.Location = new System.Drawing.Point(1263, 8);
+            this.hint_pbox.Name = "hint_pbox";
+            this.hint_pbox.Size = new System.Drawing.Size(44, 34);
+            this.hint_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hint_pbox.TabIndex = 21;
+            this.hint_pbox.TabStop = false;
+            this.hint_pbox.Click += new System.EventHandler(this.hint_pbox_Click);
+            // 
+            // hint_lbl
+            // 
+            this.hint_lbl.AutoSize = true;
+            this.hint_lbl.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hint_lbl.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.hint_lbl.Location = new System.Drawing.Point(1304, 8);
+            this.hint_lbl.Name = "hint_lbl";
+            this.hint_lbl.Size = new System.Drawing.Size(58, 34);
+            this.hint_lbl.TabIndex = 22;
+            this.hint_lbl.Text = "Hint";
+            // 
             // Old_Bldg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1436, 639);
+            this.Controls.Add(this.viewmenu_panel);
+            this.Controls.Add(this.hint_panel);
             this.Controls.Add(this.obcontainer_panel);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Old_Bldg";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Old_Bldg";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.viewmenu_panel.ResumeLayout(false);
+            this.viewmenu_panel.PerformLayout();
+            this.return_panel.ResumeLayout(false);
+            this.return_panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.achievs_panel.ResumeLayout(false);
+            this.achievs_panel.PerformLayout();
+            this.tasks_panel.ResumeLayout(false);
+            this.tasks_panel.PerformLayout();
+            this.profile_panel.ResumeLayout(false);
+            this.hint_panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.menu_pbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hint_pbox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +318,23 @@
 
         private System.Windows.Forms.Panel panel1;
         public System.Windows.Forms.Panel obcontainer_panel;
+        private System.Windows.Forms.Panel viewmenu_panel;
+        private System.Windows.Forms.Panel return_panel;
+        private System.Windows.Forms.Label return_label;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel achievs_panel;
+        private System.Windows.Forms.Label achievs_lbl;
+        private System.Windows.Forms.Panel tasks_panel;
+        private System.Windows.Forms.Label tasks_lbl;
+        private System.Windows.Forms.Panel profile_panel;
+        private System.Windows.Forms.Label ign_lbl;
+        private System.Windows.Forms.Panel hint_panel;
+        private System.Windows.Forms.Label hinttext_lbl;
+        private System.Windows.Forms.PictureBox menu_pbox;
+        private System.Windows.Forms.Label day_lbl;
+        private System.Windows.Forms.Label currency_lbl;
+        private System.Windows.Forms.PictureBox hint_pbox;
+        private System.Windows.Forms.Label hint_lbl;
     }
 }
--- bsu-tnue_lipa_rpg/Old_Bldg.cs
+++ bsu-tnue_lipa_rpg/Old_Bldg.cs
@@ -25,5 +25,115 @@ namespace bsu_tnue_lipa_rpg
             }
             obcontainer_panel.Controls.Add(OB_firstflr.INSTANCE);
         }
+
+        bool openMenu = false;
+        bool openHint = false;
+
+        #region menu and hint events
+        private void menu_pbox_Click(object sender, EventArgs e)
+        {
+            if (openMenu == false)
+            {
+                openMenu = true;
+                viewmenu_panel.Visible = true;
+                viewmenu_panel.BringToFront();
+
+                if (OB_firstflr.INSTANCE.Visible)
+                {
+                    OB_firstflr.INSTANCE.obfirstWalkTimer.Stop();
+                }
+                else if (OB_secondflr.INSTANCE.Visible)
+                {
+                    OB_secondflr.INSTANCE.obsecondWalkTimer.Stop();
+                }
+                else if (OB_thirdflr.INSTANCE.Visible)
+                {
+                    OB_thirdflr.INSTANCE.obthirdWalkTimer.Stop();
+                }
+                else if (OB_fourthflr.INSTANCE.Visible)
+                {
+                    OB_fourthflr.INSTANCE.obfourthWalkTimer.Stop();
+                }
+                else
+                {
+                    OB_fifthflr.INSTANCE.obfifthWalkTimer.Stop();
+                }
+            }
+            else
+            {
+                openMenu = false;
+                viewmenu_panel.Visible = false;
+
+
+                if (OB_firstflr.INSTANCE.Visible)
+                {
+                    OB_firstflr.INSTANCE.obfirstWalkTimer.Start();
+                }
+                else if (OB_secondflr.INSTANCE.Visible)
+                {
+                    OB_secondflr.INSTANCE.obsecondWalkTimer.Start();
+                }
+                else if (OB_thirdflr.INSTANCE.Visible)
+                {
+                    OB_thirdflr.INSTANCE.obthirdWalkTimer.Start();
+                }
+                else if (OB_fourthflr.INSTANCE.Visible)
+                {
+                    OB_fourthflr.INSTANCE.obfourthWalkTimer.Start();
+                }
+                else
+                {
+                    OB_fifthflr.INSTANCE.obfifthWalkTimer.Start();
+                }
+            }
+        }
+
+        private void hint_pbox_Click(object sender, EventArgs e)
+        {
+            if (openHint == false)
+            {
+                openHint = true;
+                hint_panel.Visible = true;
+                hint_panel.BringToFront();
+            }
+            else
+            {
+                openHint = false;
+                hint_panel.Visible = false;
+            }
+        }
+        #endregion
+
+        #region hover menu events
+        private void tasks_hoverin(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverChange(tasks_panel, tasks_lbl);
+        }
+
+        private void tasks_hoverout(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverReset(tasks_panel, tasks_lbl);
+        }
+
+        private void achievs_hoverin(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverChange(achievs_panel, achievs_lbl);
+        }
+
+        private void achievs_hoverout(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverReset(achievs_panel, achievs_lbl);
+        }
+
+        private void return_hoverin(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverChange(return_panel, return_label);
+        }
+
+        private void return_hoverout(object sender, EventArgs e)
+        {
+            Bedroom.instance.hoverReset(return_panel, return_label);
+        }
+        #endregion
     }
 }
