f6894a438f396737101d37f016cfd51184fc628c
Bernadette Awatin <badet.awatin@gmail.com>
2023-12-04 13:41:06 +0800

    Add achievements form base connection

--- bsu-tnue_lipa_rpg/Menu_options_forms/Achievements.Designer.cs
+++ bsu-tnue_lipa_rpg/Menu_options_forms/Achievements.Designer.cs
@@ -28,7 +28,286 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.close_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.a1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.a2 = new System.Windows.Forms.Label();
+            this.a3 = new System.Windows.Forms.Label();
+            this.a4 = new System.Windows.Forms.Label();
+            this.a5 = new System.Windows.Forms.Label();
+            this.a6 = new System.Windows.Forms.Label();
+            this.a7 = new System.Windows.Forms.Label();
+            this.a8 = new System.Windows.Forms.Label();
+            this.a9 = new System.Windows.Forms.Label();
+            this.a10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.ac1 = new System.Windows.Forms.Label();
+            this.ac2 = new System.Windows.Forms.Label();
+            this.ac3 = new System.Windows.Forms.Label();
+            this.ac4 = new System.Windows.Forms.Label();
+            this.ac5 = new System.Windows.Forms.Label();
+            this.ac6 = new System.Windows.Forms.Label();
+            this.ac7 = new System.Windows.Forms.Label();
+            this.ac8 = new System.Windows.Forms.Label();
+            this.ac9 = new System.Windows.Forms.Label();
+            this.ac10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // close_btn
+            // 
+            this.close_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close_btn.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close_btn.Location = new System.Drawing.Point(199, 433);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(116, 40);
+            this.close_btn.TabIndex = 14;
+            this.close_btn.Text = "CLOSE";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(165, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 46);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Achievements";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.ForeColor = System.Drawing.Color.White;
+            this.a1.Location = new System.Drawing.Point(101, 52);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(310, 34);
+            this.a1.TabIndex = 16;
+            this.a1.Text = "achv";
+            this.a1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(30, 18);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(451, 34);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "___________________________________________";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.ForeColor = System.Drawing.Color.White;
+            this.a2.Location = new System.Drawing.Point(101, 86);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(310, 34);
+            this.a2.TabIndex = 18;
+            this.a2.Text = "achv";
+            this.a2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.ForeColor = System.Drawing.Color.White;
+            this.a3.Location = new System.Drawing.Point(101, 120);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(310, 34);
+            this.a3.TabIndex = 19;
+            this.a3.Text = "achv";
+            this.a3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a4
+            // 
+            this.a4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a4.ForeColor = System.Drawing.Color.White;
+            this.a4.Location = new System.Drawing.Point(101, 154);
+            this.a4.Name = "a4";
+            this.a4.Size = new System.Drawing.Size(310, 34);
+            this.a4.TabIndex = 20;
+            this.a4.Text = "achv";
+            this.a4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a5
+            // 
+            this.a5.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a5.ForeColor = System.Drawing.Color.White;
+            this.a5.Location = new System.Drawing.Point(101, 188);
+            this.a5.Name = "a5";
+            this.a5.Size = new System.Drawing.Size(310, 34);
+            this.a5.TabIndex = 21;
+            this.a5.Text = "achv";
+            this.a5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a6
+            // 
+            this.a6.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a6.ForeColor = System.Drawing.Color.White;
+            this.a6.Location = new System.Drawing.Point(101, 222);
+            this.a6.Name = "a6";
+            this.a6.Size = new System.Drawing.Size(310, 34);
+            this.a6.TabIndex = 22;
+            this.a6.Text = "achv";
+            this.a6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a7
+            // 
+            this.a7.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a7.ForeColor = System.Drawing.Color.White;
+            this.a7.Location = new System.Drawing.Point(101, 256);
+            this.a7.Name = "a7";
+            this.a7.Size = new System.Drawing.Size(310, 34);
+            this.a7.TabIndex = 23;
+            this.a7.Text = "achv";
+            this.a7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a8
+            // 
+            this.a8.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a8.ForeColor = System.Drawing.Color.White;
+            this.a8.Location = new System.Drawing.Point(101, 290);
+            this.a8.Name = "a8";
+            this.a8.Size = new System.Drawing.Size(310, 34);
+            this.a8.TabIndex = 24;
+            this.a8.Text = "achv";
+            this.a8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a9
+            // 
+            this.a9.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a9.ForeColor = System.Drawing.Color.White;
+            this.a9.Location = new System.Drawing.Point(101, 324);
+            this.a9.Name = "a9";
+            this.a9.Size = new System.Drawing.Size(310, 34);
+            this.a9.TabIndex = 25;
+            this.a9.Text = "achv";
+            this.a9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // a10
+            // 
+            this.a10.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a10.ForeColor = System.Drawing.Color.White;
+            this.a10.Location = new System.Drawing.Point(101, 358);
+            this.a10.Name = "a10";
+            this.a10.Size = new System.Drawing.Size(310, 34);
+            this.a10.TabIndex = 26;
+            this.a10.Text = "achv";
+            this.a10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(30, 372);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(451, 34);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "___________________________________________";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ac1
+            // 
+            this.ac1.AutoSize = true;
+            this.ac1.ForeColor = System.Drawing.Color.White;
+            this.ac1.Location = new System.Drawing.Point(63, 60);
+            this.ac1.Name = "ac1";
+            this.ac1.Size = new System.Drawing.Size(19, 22);
+            this.ac1.TabIndex = 28;
+            this.ac1.Text = "x";
+            // 
+            // ac2
+            // 
+            this.ac2.AutoSize = true;
+            this.ac2.ForeColor = System.Drawing.Color.White;
+            this.ac2.Location = new System.Drawing.Point(63, 94);
+            this.ac2.Name = "ac2";
+            this.ac2.Size = new System.Drawing.Size(19, 22);
+            this.ac2.TabIndex = 29;
+            this.ac2.Text = "x";
+            // 
+            // ac3
+            // 
+            this.ac3.AutoSize = true;
+            this.ac3.ForeColor = System.Drawing.Color.White;
+            this.ac3.Location = new System.Drawing.Point(63, 128);
+            this.ac3.Name = "ac3";
+            this.ac3.Size = new System.Drawing.Size(19, 22);
+            this.ac3.TabIndex = 30;
+            this.ac3.Text = "x";
+            // 
+            // ac4
+            // 
+            this.ac4.AutoSize = true;
+            this.ac4.ForeColor = System.Drawing.Color.White;
+            this.ac4.Location = new System.Drawing.Point(63, 162);
+            this.ac4.Name = "ac4";
+            this.ac4.Size = new System.Drawing.Size(19, 22);
+            this.ac4.TabIndex = 31;
+            this.ac4.Text = "x";
+            // 
+            // ac5
+            // 
+            this.ac5.AutoSize = true;
+            this.ac5.ForeColor = System.Drawing.Color.White;
+            this.ac5.Location = new System.Drawing.Point(63, 196);
+            this.ac5.Name = "ac5";
+            this.ac5.Size = new System.Drawing.Size(19, 22);
+            this.ac5.TabIndex = 32;
+            this.ac5.Text = "x";
+            // 
+            // ac6
+            // 
+            this.ac6.AutoSize = true;
+            this.ac6.ForeColor = System.Drawing.Color.White;
+            this.ac6.Location = new System.Drawing.Point(63, 230);
+            this.ac6.Name = "ac6";
+            this.ac6.Size = new System.Drawing.Size(19, 22);
+            this.ac6.TabIndex = 33;
+            this.ac6.Text = "x";
+            // 
+            // ac7
+            // 
+            this.ac7.AutoSize = true;
+            this.ac7.ForeColor = System.Drawing.Color.White;
+            this.ac7.Location = new System.Drawing.Point(63, 268);
+            this.ac7.Name = "ac7";
+            this.ac7.Size = new System.Drawing.Size(19, 22);
+            this.ac7.TabIndex = 34;
+            this.ac7.Text = "x";
+            // 
+            // ac8
+            // 
+            this.ac8.AutoSize = true;
+            this.ac8.ForeColor = System.Drawing.Color.White;
+            this.ac8.Location = new System.Drawing.Point(63, 298);
+            this.ac8.Name = "ac8";
+            this.ac8.Size = new System.Drawing.Size(19, 22);
+            this.ac8.TabIndex = 35;
+            this.ac8.Text = "x";
+            // 
+            // ac9
+            // 
+            this.ac9.AutoSize = true;
+            this.ac9.ForeColor = System.Drawing.Color.White;
+            this.ac9.Location = new System.Drawing.Point(63, 332);
+            this.ac9.Name = "ac9";
+            this.ac9.Size = new System.Drawing.Size(19, 22);
+            this.ac9.TabIndex = 36;
+            this.ac9.Text = "x";
+            // 
+            // ac10
+            // 
+            this.ac10.AutoSize = true;
+            this.ac10.ForeColor = System.Drawing.Color.White;
+            this.ac10.Location = new System.Drawing.Point(63, 366);
+            this.ac10.Name = "ac10";
+            this.ac10.Size = new System.Drawing.Size(19, 22);
+            this.ac10.TabIndex = 37;
+            this.ac10.Text = "x";
             // 
             // Achievements
             // 
@@ -36,16 +315,66 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
             this.ClientSize = new System.Drawing.Size(514, 485);
+            this.Controls.Add(this.ac10);
+            this.Controls.Add(this.ac9);
+            this.Controls.Add(this.ac8);
+            this.Controls.Add(this.ac7);
+            this.Controls.Add(this.ac6);
+            this.Controls.Add(this.ac5);
+            this.Controls.Add(this.ac4);
+            this.Controls.Add(this.ac3);
+            this.Controls.Add(this.ac2);
+            this.Controls.Add(this.ac1);
+            this.Controls.Add(this.a10);
+            this.Controls.Add(this.a9);
+            this.Controls.Add(this.a8);
+            this.Controls.Add(this.a7);
+            this.Controls.Add(this.a6);
+            this.Controls.Add(this.a5);
+            this.Controls.Add(this.a4);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Achievements";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Achievements";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label a2;
+        private System.Windows.Forms.Label a3;
+        private System.Windows.Forms.Label a4;
+        private System.Windows.Forms.Label a5;
+        private System.Windows.Forms.Label a6;
+        private System.Windows.Forms.Label a7;
+        private System.Windows.Forms.Label a8;
+        private System.Windows.Forms.Label a9;
+        private System.Windows.Forms.Label a10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label ac1;
+        private System.Windows.Forms.Label ac2;
+        private System.Windows.Forms.Label ac3;
+        private System.Windows.Forms.Label ac4;
+        private System.Windows.Forms.Label ac5;
+        private System.Windows.Forms.Label ac6;
+        private System.Windows.Forms.Label ac7;
+        private System.Windows.Forms.Label ac8;
+        private System.Windows.Forms.Label ac9;
+        private System.Windows.Forms.Label ac10;
     }
 }
--- bsu-tnue_lipa_rpg/Menu_options_forms/Achievements.cs
+++ bsu-tnue_lipa_rpg/Menu_options_forms/Achievements.cs
@@ -15,6 +15,67 @@ namespace bsu_tnue_lipa_rpg.Menu_options_forms
         public Achievements()
         {
             InitializeComponent();
+            if (Bedroom.instance.Visible)
+            {
+                Bedroom.instance.Enabled = false;
+            }
+            else if (Facade.instance.Visible)
+            {
+                Facade.instance.Enabled = false;
+            }
+            else if (Map.instance.Visible)
+            {
+                Map.instance.Enabled = false;
+            }
+            else if (CECS_bldg.instance.Visible)
+            {
+                CECS_bldg.instance.Enabled = false;
+            }
+            else if (Old_Bldg.instance.Visible)
+            {
+                Old_Bldg.instance.Enabled = false;
+            }
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            if (Bedroom.instance.Visible)
+            {
+                Bedroom.instance.achievsClicked = false;
+                Bedroom.instance.hoverReset(Bedroom.instance.achievs_lbl);
+                Bedroom.instance.Enabled = true;
+            }
+
+            else if (Facade.instance.Visible)
+            {
+                Facade.instance.achievsClicked = false;
+                Bedroom.instance.hoverReset(Facade.instance.achievs_lbl);
+                Facade.instance.Enabled = true;
+            }
+
+            else if (Map.instance.Visible)
+            {
+                Map.instance.achievsClicked = false;
+                Bedroom.instance.hoverReset(Map.instance.achievs_lbl);
+                Map.instance.Enabled = true;
+            }
+
+
+            else if (CECS_bldg.instance.Visible)
+            {
+                CECS_bldg.instance.achievsClicked = false;
+                Bedroom.instance.hoverReset(CECS_bldg.instance.achievs_lbl);
+                CECS_bldg.instance.Enabled = true;
+            }
+
+            else if (Old_Bldg.instance.Visible)
+            {
+                Old_Bldg.instance.achievsClicked = false;
+                Bedroom.instance.hoverReset(Old_Bldg.instance.achievs_lbl);
+                Old_Bldg.instance.Enabled = true;
+            }
+
+            this.Close();
         }
     }
 }
