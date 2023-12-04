f48e8b437a1e24becc1eadd1efb04ad2b1ff8ddc
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-12-04 10:24:30 +0800

    Modify the labels in the top.cs

--- bsu-tnue_lipa_rpg/Closet_garments_uc/top.Designer.cs
+++ bsu-tnue_lipa_rpg/Closet_garments_uc/top.Designer.cs
@@ -49,36 +49,40 @@
             // top1_lbl
             // 
             this.top1_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.top1_lbl.Location = new System.Drawing.Point(43, 153);
+            this.top1_lbl.Location = new System.Drawing.Point(32, 124);
+            this.top1_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top1_lbl.Name = "top1_lbl";
-            this.top1_lbl.Size = new System.Drawing.Size(145, 34);
+            this.top1_lbl.Size = new System.Drawing.Size(109, 28);
             this.top1_lbl.TabIndex = 9;
             this.top1_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // top3_lbl
             // 
             this.top3_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.top3_lbl.Location = new System.Drawing.Point(451, 153);
+            this.top3_lbl.Location = new System.Drawing.Point(338, 124);
+            this.top3_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top3_lbl.Name = "top3_lbl";
-            this.top3_lbl.Size = new System.Drawing.Size(181, 43);
+            this.top3_lbl.Size = new System.Drawing.Size(136, 35);
             this.top3_lbl.TabIndex = 10;
             this.top3_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // top2_lbl
             // 
             this.top2_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.top2_lbl.Location = new System.Drawing.Point(251, 152);
+            this.top2_lbl.Location = new System.Drawing.Point(188, 124);
+            this.top2_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top2_lbl.Name = "top2_lbl";
-            this.top2_lbl.Size = new System.Drawing.Size(181, 43);
+            this.top2_lbl.Size = new System.Drawing.Size(136, 35);
             this.top2_lbl.TabIndex = 10;
             this.top2_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // top4_lbl
             // 
             this.top4_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.top4_lbl.Location = new System.Drawing.Point(49, 395);
+            this.top4_lbl.Location = new System.Drawing.Point(37, 321);
+            this.top4_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top4_lbl.Name = "top4_lbl";
-            this.top4_lbl.Size = new System.Drawing.Size(139, 30);
+            this.top4_lbl.Size = new System.Drawing.Size(104, 24);
             this.top4_lbl.TabIndex = 11;
             this.top4_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.top4_lbl.Click += new System.EventHandler(this.top4_lbl_Click);
@@ -86,10 +90,12 @@
             // top1_desc
             // 
             this.top1_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.top1_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.top1_desc.Location = new System.Drawing.Point(53, 196);
+            this.top1_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top1_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.top1_desc.Location = new System.Drawing.Point(40, 159);
+            this.top1_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top1_desc.Name = "top1_desc";
-            this.top1_desc.Size = new System.Drawing.Size(122, 56);
+            this.top1_desc.Size = new System.Drawing.Size(92, 46);
             this.top1_desc.TabIndex = 12;
             this.top1_desc.Text = "Unlock";
             this.top1_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -98,10 +104,12 @@
             // top2_desc
             // 
             this.top2_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.top2_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.top2_desc.Location = new System.Drawing.Point(273, 196);
+            this.top2_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top2_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.top2_desc.Location = new System.Drawing.Point(205, 159);
+            this.top2_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top2_desc.Name = "top2_desc";
-            this.top2_desc.Size = new System.Drawing.Size(122, 56);
+            this.top2_desc.Size = new System.Drawing.Size(92, 46);
             this.top2_desc.TabIndex = 13;
             this.top2_desc.Text = "Unlock";
             this.top2_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -110,10 +118,12 @@
             // top3_desc
             // 
             this.top3_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.top3_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.top3_desc.Location = new System.Drawing.Point(480, 196);
+            this.top3_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top3_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.top3_desc.Location = new System.Drawing.Point(360, 159);
+            this.top3_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top3_desc.Name = "top3_desc";
-            this.top3_desc.Size = new System.Drawing.Size(122, 56);
+            this.top3_desc.Size = new System.Drawing.Size(92, 46);
             this.top3_desc.TabIndex = 14;
             this.top3_desc.Text = "Unlock";
             this.top3_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,10 +132,12 @@
             // top4_desc
             // 
             this.top4_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.top4_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.top4_desc.Location = new System.Drawing.Point(53, 425);
+            this.top4_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold Condensed", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top4_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.top4_desc.Location = new System.Drawing.Point(40, 345);
+            this.top4_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.top4_desc.Name = "top4_desc";
-            this.top4_desc.Size = new System.Drawing.Size(122, 56);
+            this.top4_desc.Size = new System.Drawing.Size(92, 46);
             this.top4_desc.TabIndex = 15;
             this.top4_desc.Text = "Unlock";
             this.top4_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -136,9 +148,10 @@
             this.top3_pbox.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.top3_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.top3_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.CICS_Org_Shirt;
-            this.top3_pbox.Location = new System.Drawing.Point(479, 37);
+            this.top3_pbox.Location = new System.Drawing.Point(359, 30);
+            this.top3_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.top3_pbox.Name = "top3_pbox";
-            this.top3_pbox.Size = new System.Drawing.Size(122, 113);
+            this.top3_pbox.Size = new System.Drawing.Size(92, 93);
             this.top3_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.top3_pbox.TabIndex = 8;
             this.top3_pbox.TabStop = false;
@@ -149,9 +162,10 @@
             this.top4_pbox.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.top4_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.top4_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.Civilian_Top_2;
-            this.top4_pbox.Location = new System.Drawing.Point(53, 269);
+            this.top4_pbox.Location = new System.Drawing.Point(40, 219);
+            this.top4_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.top4_pbox.Name = "top4_pbox";
-            this.top4_pbox.Size = new System.Drawing.Size(122, 113);
+            this.top4_pbox.Size = new System.Drawing.Size(92, 93);
             this.top4_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.top4_pbox.TabIndex = 7;
             this.top4_pbox.TabStop = false;
@@ -162,9 +176,10 @@
             this.top2_pbox.BackColor = System.Drawing.Color.DarkGray;
             this.top2_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.top2_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.PE_T_Shirt;
-            this.top2_pbox.Location = new System.Drawing.Point(269, 37);
+            this.top2_pbox.Location = new System.Drawing.Point(202, 30);
+            this.top2_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.top2_pbox.Name = "top2_pbox";
-            this.top2_pbox.Size = new System.Drawing.Size(122, 113);
+            this.top2_pbox.Size = new System.Drawing.Size(92, 93);
             this.top2_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.top2_pbox.TabIndex = 6;
             this.top2_pbox.TabStop = false;
@@ -175,9 +190,10 @@
             this.top1_pbox.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.top1_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.top1_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.College_Barong;
-            this.top1_pbox.Location = new System.Drawing.Point(53, 37);
+            this.top1_pbox.Location = new System.Drawing.Point(40, 30);
+            this.top1_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.top1_pbox.Name = "top1_pbox";
-            this.top1_pbox.Size = new System.Drawing.Size(122, 113);
+            this.top1_pbox.Size = new System.Drawing.Size(92, 93);
             this.top1_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.top1_pbox.TabIndex = 5;
             this.top1_pbox.TabStop = false;
@@ -185,7 +201,7 @@
             // 
             // top
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(128)))), ((int)(((byte)(97)))));
             this.Controls.Add(this.top4_desc);
@@ -200,8 +216,9 @@
             this.Controls.Add(this.top4_pbox);
             this.Controls.Add(this.top2_pbox);
             this.Controls.Add(this.top1_pbox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "top";
-            this.Size = new System.Drawing.Size(671, 518);
+            this.Size = new System.Drawing.Size(503, 421);
             ((System.ComponentModel.ISupportInitialize)(this.top3_pbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.top4_pbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.top2_pbox)).EndInit();
