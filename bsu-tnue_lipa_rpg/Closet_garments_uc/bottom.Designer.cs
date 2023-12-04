5ed1592e23b894d44aa54faf4fe2631111b94f7b
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-12-04 10:19:07 +0800

    Modify the design of the Labels in bottom.cs

--- bsu-tnue_lipa_rpg/Closet_garments_uc/bottom.Designer.cs
+++ bsu-tnue_lipa_rpg/Closet_garments_uc/bottom.Designer.cs
@@ -49,10 +49,12 @@
             // bot4_desc
             // 
             this.bot4_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.bot4_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.bot4_desc.Location = new System.Drawing.Point(61, 425);
+            this.bot4_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bot4_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.bot4_desc.Location = new System.Drawing.Point(46, 345);
+            this.bot4_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot4_desc.Name = "bot4_desc";
-            this.bot4_desc.Size = new System.Drawing.Size(122, 56);
+            this.bot4_desc.Size = new System.Drawing.Size(92, 46);
             this.bot4_desc.TabIndex = 27;
             this.bot4_desc.Text = "Unlock";
             this.bot4_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -61,10 +63,12 @@
             // bot3_desc
             // 
             this.bot3_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.bot3_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.bot3_desc.Location = new System.Drawing.Point(488, 196);
+            this.bot3_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bot3_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.bot3_desc.Location = new System.Drawing.Point(366, 159);
+            this.bot3_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot3_desc.Name = "bot3_desc";
-            this.bot3_desc.Size = new System.Drawing.Size(122, 56);
+            this.bot3_desc.Size = new System.Drawing.Size(92, 46);
             this.bot3_desc.TabIndex = 26;
             this.bot3_desc.Text = "Unlock";
             this.bot3_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,10 +77,12 @@
             // bot2_desc
             // 
             this.bot2_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.bot2_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.bot2_desc.Location = new System.Drawing.Point(281, 196);
+            this.bot2_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bot2_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.bot2_desc.Location = new System.Drawing.Point(211, 159);
+            this.bot2_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot2_desc.Name = "bot2_desc";
-            this.bot2_desc.Size = new System.Drawing.Size(122, 56);
+            this.bot2_desc.Size = new System.Drawing.Size(92, 46);
             this.bot2_desc.TabIndex = 25;
             this.bot2_desc.Text = "Unlock";
             this.bot2_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -85,10 +91,12 @@
             // bot1_desc
             // 
             this.bot1_desc.BackColor = System.Drawing.Color.IndianRed;
-            this.bot1_desc.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.bot1_desc.Location = new System.Drawing.Point(61, 196);
+            this.bot1_desc.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bot1_desc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.bot1_desc.Location = new System.Drawing.Point(46, 159);
+            this.bot1_desc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot1_desc.Name = "bot1_desc";
-            this.bot1_desc.Size = new System.Drawing.Size(122, 56);
+            this.bot1_desc.Size = new System.Drawing.Size(92, 46);
             this.bot1_desc.TabIndex = 24;
             this.bot1_desc.Text = "Unlock";
             this.bot1_desc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -97,9 +105,10 @@
             // bot4_lbl
             // 
             this.bot4_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bot4_lbl.Location = new System.Drawing.Point(44, 385);
+            this.bot4_lbl.Location = new System.Drawing.Point(33, 313);
+            this.bot4_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot4_lbl.Name = "bot4_lbl";
-            this.bot4_lbl.Size = new System.Drawing.Size(149, 40);
+            this.bot4_lbl.Size = new System.Drawing.Size(112, 32);
             this.bot4_lbl.TabIndex = 23;
             this.bot4_lbl.Text = "Shorts";
             this.bot4_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -107,9 +116,10 @@
             // bot2_lbl
             // 
             this.bot2_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bot2_lbl.Location = new System.Drawing.Point(263, 153);
+            this.bot2_lbl.Location = new System.Drawing.Point(197, 124);
+            this.bot2_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot2_lbl.Name = "bot2_lbl";
-            this.bot2_lbl.Size = new System.Drawing.Size(162, 43);
+            this.bot2_lbl.Size = new System.Drawing.Size(122, 35);
             this.bot2_lbl.TabIndex = 21;
             this.bot2_lbl.Text = "PE Jogging Pants";
             this.bot2_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -117,9 +127,10 @@
             // bot3_lbl
             // 
             this.bot3_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bot3_lbl.Location = new System.Drawing.Point(509, 153);
+            this.bot3_lbl.Location = new System.Drawing.Point(365, 124);
+            this.bot3_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot3_lbl.Name = "bot3_lbl";
-            this.bot3_lbl.Size = new System.Drawing.Size(125, 42);
+            this.bot3_lbl.Size = new System.Drawing.Size(94, 34);
             this.bot3_lbl.TabIndex = 22;
             this.bot3_lbl.Text = "Denim Pants";
             this.bot3_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,9 +138,10 @@
             // bot1_lbl
             // 
             this.bot1_lbl.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bot1_lbl.Location = new System.Drawing.Point(66, 153);
+            this.bot1_lbl.Location = new System.Drawing.Point(38, 124);
+            this.bot1_lbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.bot1_lbl.Name = "bot1_lbl";
-            this.bot1_lbl.Size = new System.Drawing.Size(143, 43);
+            this.bot1_lbl.Size = new System.Drawing.Size(107, 35);
             this.bot1_lbl.TabIndex = 20;
             this.bot1_lbl.Text = "College Pants";
             this.bot1_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -139,9 +151,10 @@
             this.bottom3_pbox.BackColor = System.Drawing.Color.DarkGray;
             this.bottom3_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.bottom3_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.Denim_Pants;
-            this.bottom3_pbox.Location = new System.Drawing.Point(487, 37);
+            this.bottom3_pbox.Location = new System.Drawing.Point(365, 30);
+            this.bottom3_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bottom3_pbox.Name = "bottom3_pbox";
-            this.bottom3_pbox.Size = new System.Drawing.Size(122, 113);
+            this.bottom3_pbox.Size = new System.Drawing.Size(92, 93);
             this.bottom3_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.bottom3_pbox.TabIndex = 19;
             this.bottom3_pbox.TabStop = false;
@@ -152,9 +165,10 @@
             this.bottom4_pbox.BackColor = System.Drawing.Color.DarkGray;
             this.bottom4_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.bottom4_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.Civilian_Bottom_2;
-            this.bottom4_pbox.Location = new System.Drawing.Point(61, 269);
+            this.bottom4_pbox.Location = new System.Drawing.Point(46, 219);
+            this.bottom4_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bottom4_pbox.Name = "bottom4_pbox";
-            this.bottom4_pbox.Size = new System.Drawing.Size(122, 113);
+            this.bottom4_pbox.Size = new System.Drawing.Size(92, 93);
             this.bottom4_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.bottom4_pbox.TabIndex = 18;
             this.bottom4_pbox.TabStop = false;
@@ -165,9 +179,10 @@
             this.bottom2_pbox.BackColor = System.Drawing.Color.DarkGray;
             this.bottom2_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.bottom2_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.PE_Jogging_Pant;
-            this.bottom2_pbox.Location = new System.Drawing.Point(277, 37);
+            this.bottom2_pbox.Location = new System.Drawing.Point(208, 30);
+            this.bottom2_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bottom2_pbox.Name = "bottom2_pbox";
-            this.bottom2_pbox.Size = new System.Drawing.Size(122, 113);
+            this.bottom2_pbox.Size = new System.Drawing.Size(92, 93);
             this.bottom2_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.bottom2_pbox.TabIndex = 17;
             this.bottom2_pbox.TabStop = false;
@@ -178,9 +193,10 @@
             this.bottom1_pbox.BackColor = System.Drawing.Color.DarkGray;
             this.bottom1_pbox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.bottom1_pbox.Image = global::bsu_tnue_lipa_rpg.Properties.Resources.College_Pants;
-            this.bottom1_pbox.Location = new System.Drawing.Point(61, 37);
+            this.bottom1_pbox.Location = new System.Drawing.Point(46, 30);
+            this.bottom1_pbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bottom1_pbox.Name = "bottom1_pbox";
-            this.bottom1_pbox.Size = new System.Drawing.Size(122, 113);
+            this.bottom1_pbox.Size = new System.Drawing.Size(92, 93);
             this.bottom1_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.bottom1_pbox.TabIndex = 16;
             this.bottom1_pbox.TabStop = false;
@@ -188,7 +204,7 @@
             // 
             // bottom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(128)))), ((int)(((byte)(97)))));
             this.Controls.Add(this.bot4_desc);
@@ -203,8 +219,9 @@
             this.Controls.Add(this.bottom4_pbox);
             this.Controls.Add(this.bottom2_pbox);
             this.Controls.Add(this.bottom1_pbox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "bottom";
-            this.Size = new System.Drawing.Size(839, 648);
+            this.Size = new System.Drawing.Size(629, 526);
             ((System.ComponentModel.ISupportInitialize)(this.bottom3_pbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bottom4_pbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bottom2_pbox)).EndInit();
@@ -214,11 +231,6 @@
         }
 
         #endregion
-
-        public System.Windows.Forms.Label bot4_desc;
-        public System.Windows.Forms.Label bot3_desc;
-        public System.Windows.Forms.Label bot2_desc;
-        public System.Windows.Forms.Label bot1_desc;
         public System.Windows.Forms.Label bot4_lbl;
         public System.Windows.Forms.Label bot2_lbl;
         public System.Windows.Forms.Label bot3_lbl;
@@ -227,5 +239,9 @@
         public System.Windows.Forms.PictureBox bottom4_pbox;
         public System.Windows.Forms.PictureBox bottom2_pbox;
         public System.Windows.Forms.PictureBox bottom1_pbox;
+        public System.Windows.Forms.Label bot4_desc;
+        public System.Windows.Forms.Label bot3_desc;
+        public System.Windows.Forms.Label bot2_desc;
+        public System.Windows.Forms.Label bot1_desc;
     }
 }
